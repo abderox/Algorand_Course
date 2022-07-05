b5fbf648040a92af812202ab51cf6232e576740c
abderox <abdelhadi12mouzafir@gmail.com>
2022-07-05 02:20:47 +0100

    ALGORAND LESSON 3  : creating account using kmd

--- account_kmd_create.js
+++ account_kmd_create.js
@@ -7,16 +7,42 @@ const KmdClient = new algosdk.Kmd(process.env.KMD_TOKEN,
     process.env.KMD_SERVER,
     process.env.KMD_PORT);
 
-const createWallet = async (walllrtName, walletPass) => {
+const createWallet = async (walletName, walletPass) => {
+
     console.log("############### creating wallet")
-    const walletId = await KmdClient.createWallet(walllrtName, walletPass, "", "sqlite");
+    const walletId = (await KmdClient.createWallet(walletName, walletPass, "", "sqlite")).wallet.id;
     console.log("🚀 ~ file: account_kmd_create.js ~ line 12 ~ createWal ~ walletId", walletId)
-     
+    const walletHandle = (await KmdClient.initWalletHandle(walletId, walletPass)).wallet_handle_token;
+    console.log("🚀 ~ file: account_kmd_create.js ~ line 15 ~ createWal ~ walletHandle", walletHandle)
+
+    return walletHandle;
 }
 
-(async()=> {
-    await createWallet(process.env.KMD_WALLET_NAME,process.env.KMD_WALLET_PASS)
+const getWalletMnemonic = async (walletHandle , walletPass) =>{
+
+    console.log("######### exporting account mnemonic ")
+
+    masterDerivationKey = (await KmdClient.exportMasterDerivationKey(walletHandle, walletPassword)).master_derivation_key;
+
+}
+
+const generateKey = ascync (walletHandle) =>{
+    console.log("############### creating account")
+
+    const account = (await KmdClient.generateKey(walletHandle).address)
+    console.log("🚀 ~ file: account_kmd_create.js ~ line 25 ~ generateKey ~ account", account)
+    return account ;
+}
+
+(async () => {
+
+    const handleWallet = await createWallet(process.env.KMD_WALLET_NAME, process.env.KMD_WALLET_PASS)
+    console.log("🚀 ~ file: account_kmd_create.js ~ line 23 ~ handleWallet", handleWallet)
+
     console.log("############### wallet created ")
+    const listWallets = await KmdClient.listWallets();
+    console.log("🚀  listWallets", listWallets)
+
 
 })().catch(console.error)
 
