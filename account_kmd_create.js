3e5ad9508f68a57ee38fcb783023f2d528a5567a
abderox <abdelhadi12mouzafir@gmail.com>
2022-07-05 02:30:55 +0100

    ALGORAND LESSON 3  : creating account using kmd

--- account_kmd_create.js
+++ account_kmd_create.js
@@ -18,26 +18,34 @@ const createWallet = async (walletName, walletPass) => {
     return walletHandle;
 }
 
-const getWalletMnemonic = async (walletHandle , walletPass) =>{
+const getWalletMnemonic = async (walletHandle, walletPass) => {
 
     console.log("######### exporting account mnemonic ")
 
-    masterDerivationKey = (await KmdClient.exportMasterDerivationKey(walletHandle, walletPassword)).master_derivation_key;
+    const masterDerivationKey = (await KmdClient.exportMasterDerivationKey(walletHandle, walletPass)).master_derivation_key;
+    console.log("🚀 ~ file: account_kmd_create.js ~ line 26 ~ getWalletMnemonic ~ masterDerivationKey", masterDerivationKey)
+    walletMnemonic = algosdk.masterDerivationKeyToMnemonic(masterDerivationKey)
+    console.log("🚀 ~ file: account_kmd_create.js ~ line 28 ~ getWalletMnemonic ~ walletMnemonic", walletMnemonic)
+
 
 }
 
-const generateKey = ascync (walletHandle) =>{
+const generateKey = async (walletHandle) => {
     console.log("############### creating account")
 
     const account = (await KmdClient.generateKey(walletHandle).address)
     console.log("🚀 ~ file: account_kmd_create.js ~ line 25 ~ generateKey ~ account", account)
-    return account ;
+
 }
 
 (async () => {
 
-    const handleWallet = await createWallet(process.env.KMD_WALLET_NAME, process.env.KMD_WALLET_PASS)
-    console.log("🚀 ~ file: account_kmd_create.js ~ line 23 ~ handleWallet", handleWallet)
+    const walletPass = process.env.KMD_WALLET_PASS;
+    const walletName = process.env.KMD_WALLET_NAME;
+
+    const handleWallet = await createWallet(walletName, walletPass)
+    await generateKey(handleWallet);
+    await getWalletMnemonic(handleWallet, walletPass)
 
     console.log("############### wallet created ")
     const listWallets = await KmdClient.listWallets();
