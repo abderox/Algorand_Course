563fb8ac68dbbb972e3de2ca3d99d13a1f16681e
abderox <abdelhadi12mouzafir@gmail.com>
2022-07-06 18:31:49 +0100

    ALGORAND LESSON 3 : update standalone account

--- account_standalone.js
+++ account_standalone.js
@@ -4,12 +4,13 @@ dotenv.config();
 
 
 
-(async () => {
+module.exports.generateStandaloneAccount = async () => {
 
     // ? Standalone account
     let account = algosdk.generateAccount()
-    console.log("🚀 ~ file: account_kmd_import.js ~ line 32 ~ account", account)
+    console.log("🚀 ~ file: account_standalone.js ~ line 32 ~ account", account.addr)
     let accountMnemonic = algosdk.secretKeyToMnemonic(account.sk);
-    console.log("🚀 ~ file: account_kmd_import.js ~ line 33 ~ accountMnemonic", accountMnemonic)
-   
-})();
+    console.log("🚀 ~ file: account_standalone.js ~ line 33 ~ accountMnemonic", accountMnemonic)
+    return account;
+}
+
